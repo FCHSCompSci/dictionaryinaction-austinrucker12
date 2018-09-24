b8472aacd9e10280c7ff631bb8582ae7e41289cf
Austin A Rucker <austin.rucker@stu.jefferson.kyschools.us>
2018-09-24 09:07:40 -0400

    first commit

--- project2.py
+++ project2.py
@@ -1,5 +1,42 @@
 def make_store(item, price):
-    store = {
-        'item' : item,
-        'price' : price,
+ clothes_store = {
+        'jeans': 64.00,
+        'shirt': 30.00,
+        'sweat_pants': 60.00,
+        'shorts': 20.00,
+        'socks': 10.00,
+        'graphic_tee': 40.00,
+        'sweat_shirt': 60.00,
         }
+ 
+while True:
+    item =  input("what are you buying today?")
+    print(f"your total is...")
+    total= myDict[item]
+    
+
+#    items = input("are you buying anything else?")
+#    if items == "no":
+        
+
+ 
+
+
+
+
+
+ 
+ 
+ 
+# get one item from the user, then get another. use a while loop
+# check to see what the item's cost is in the dictionary
+# then, add that cost to a running total using sum = sum + jeans_value
+# after they've given you their entire purchase, show a total.
+  
+        
+        
+
+
+
+
+
